27a9a4ca464312fd02a85ff9b396508732a547f8
meinig <165210366+matheinig@users.noreply.github.com>
2024-11-10 12:43:27 +0100

    1.1 release
    Added support for collection instancing

--- addons/Gamiflow/__init__.py
+++ addons/Gamiflow/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name"        : "Gamiflow",
     "description" : "Workflow improvements for game assets",
     "author"      : "Mathieu Einig",
-    "version"     : (0, 2),
+    "version"     : (1, 1),
     "blender"     : (3, 6, 0),
     "location"    : "Wherever",
     "warning"     : "",
