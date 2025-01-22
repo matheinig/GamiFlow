a5472ef2bc00cac2b0bab8bf4d398d5e54a1b059
meinig <165210366+matheinig@users.noreply.github.com>
2025-01-22 20:18:34 +0100

    Release 1.4
    - Added cage generation support
      - Can set per object cage offset
      - Can tweak cage offset using weight painting
      - Can flag edges as being cage-only
    - Sculpts in the high poly set can be generated as linked duplicates if no further processing is needed (faster and lighter)
    - Fixed crash caused by empty high-poly slots
    - Generated objects are always made selectable, preventing weird bugs
    -

--- addons/Gamiflow/__init__.py
+++ addons/Gamiflow/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name"        : "Gamiflow",
     "description" : "Workflow improvements for game assets",
     "author"      : "Mathieu Einig",
-    "version"     : (1, 3),
+    "version"     : (1, 4),
     "blender"     : (3, 6, 0),
     "location"    : "Wherever",
     "warning"     : "",
