e8bff43a016ce2ffabdaedbfdc9e037d5c38ace7
meinig <165210366+matheinig@users.noreply.github.com>
2025-09-03 15:45:32 +0200

    Release 1.8.0 (Armatures)
    Added support for armatures
    Added support for shape keys
    Added bake and export actions

--- addons/Gamiflow/__init__.py
+++ addons/Gamiflow/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name"        : "Gamiflow",
     "description" : "Workflow improvements for game assets",
     "author"      : "Mathieu Einig",
-    "version"     : (1, 7),
+    "version"     : (1, 8),
     "blender"     : (3, 6, 0),
     "location"    : "Wherever",
     "warning"     : "",
